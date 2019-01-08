4c2540d6e6c1c9c82dc02be530b10fb8af7b9e68
amichaiman <ami.dan.man@gmail.com>
2019-01-08 21:35:47 +0200

    bug fix

--- BloomFilter.h
+++ BloomFilter.h
@@ -11,6 +11,7 @@ class BloomFilter {
 public:
     explicit BloomFilter(unsigned int number_of_tables=3, unsigned int buckets_per_table=16, unsigned int bits_per_bucket=60, unsigned int log_max_elements_in_bucket=4);
     void insert(const char *element);
+    bool query(const char *element);
     struct InvalidAlignmentException : public exception {
         const char * what() const noexcept {
             return "bits per bucket + log max elements in bucket must be byte aligned";
@@ -26,6 +27,14 @@ public:
             return "number of bits per bucket cannot exceed sizeof(size_t)";
         }
     };
+    struct OverflowException : public exception {
+        const char * what() const noexcept {
+            return "overflow detected!";
+        }
+    };
+
+    unsigned int get_number_of_elements_in_bucket(const char *element);
+
 private:
     unsigned int number_of_tables;
     unsigned int buckets_per_table;
@@ -33,21 +42,18 @@ private:
     unsigned int log_max_elements_in_bucket;
     unsigned char *table;
     static const unsigned int BITS_IN_BYTE = 8;
-    unsigned int get_table_index_with_emptiest_bucket(const string &value);
-
+    unsigned int get_table_index_with_emptiest_bucket(const char *element);
     unsigned int get_number_of_elements_in_bucket(unsigned int table_index, size_t bucket_index);
-
     unsigned int get_table_size_in_bytes();
-
-    bool element_is_in_bloom_filter(const char *value);
-
+    bool element_is_in_bloom_filter(const char *element);
     bool bucket_contains_element(unsigned int table_index, size_t bucket_index, const char *element);
-
     size_t get_fingerprint(const char *element, unsigned int length);
+    size_t get_bitmask(unsigned int size);
+    size_t get_bucket(unsigned int table_index, size_t bucket_index);
+    unsigned int get_bucket_size_in_bytes();
+    size_t get_bucket_index(const char *element);
 
-    size_t get_element_at_index(unsigned int table_index, size_t bucket_index, unsigned int element_index);
-
-    void increment_bucket_element_counter(unsigned int table_index, size_t bucket_index);
+    size_t number_of_lost_bits(unsigned int fingerprint_length, unsigned int number_of_elements);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,30 @@
 #include "BloomFilter.h"
 
 int main() {
-    BloomFilter bloomFilter;
-    bloomFilter.insert("the name is filter. bloom filter");
+    BloomFilter bloom_filter;
+    std::ios_base::fmtflags f(cout.flags());
+    int a;
+    int number_of_elements = 300;
+    for (int i=1; i<number_of_elements; i++) {
+        bloom_filter.insert(reinterpret_cast<const char *>(&i));
+    }
+    int max = 0;
+    for (int i=1; i<number_of_elements; i++) {
+        if (bloom_filter.query(reinterpret_cast<const char *>(&i)) == 0) {
+            cout.flags(f);
+            cout <<  i << endl;
+        }
+    }
+    int n=0;
+    for (int i=500000; i<500000+number_of_elements; i++){
+        if (bloom_filter.query(reinterpret_cast<const char *>(&i)) == 1) {
+            n++;
+        }
+
+    }
+    cout.flags(f);
+    cout << n << endl;
+    cout << (1.0*n)/number_of_elements << endl;
     return 0;
 }
+
